97be4bcbd71d3f65fdf0ac6a5e2002f8240af86c
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-06-01 22:43:21 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include "sdlpp/events.hpp"
+#include "sdlpp/render.hpp"
+#include "sdlpp/window.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    auto window = sdl::Window{"game", 0, 0, 300, 300, SDL_WINDOW_SHOWN};
+    auto renderer = sdl::Renderer{window, -1, SDL_RENDERER_ACCELERATED};
+
+    for (bool running = true; running;) {
+        for (auto o = sdl::pollEvent(); o; o = sdl::pollEvent()) {
+            auto event = *o;
+
+            switch (event.type) {
+            case SDL_QUIT:
+                running = false;
+                break;
+            }
+        }
+
+        renderer.drawColor({255, 255, 255, 255});
+        renderer.drawLine(0, 0, 100, 100);
+
+        renderer.present();
+    }
+
+    return 0;
+}
